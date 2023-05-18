b6aa38cfe2eab9ffa08b7eede4f7aeb7917285b5
ranerajesh <rane.rajeshgec@gmail.com>
2023-05-18 11:36:45 +0000

    added flask changes

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route("/")
-
-
+@app.route("/")
 def hello_world():
   return "Hello, World!"
 
 
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=False)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
