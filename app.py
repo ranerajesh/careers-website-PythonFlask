82567cf3921329b98cd1441f39b4e2a8d8061ae7
ranerajesh <rane.rajeshgec@gmail.com>
2023-06-11 13:17:31 +0000

    Added job page

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ JOBS = [{
 def hello_world():
   jobs = load_jobs_from_db()
   #return render_template('home.html', jobs=JOBS, company_name='Jovian')
-  return render_template('home.html', jobs=jobs, company_name='Jovian')
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
@@ -43,7 +43,9 @@ def list_jobs():
 def show_job(id):
   print(id)
   job = load_job_from_db(id)
-  return jsonify(job)
+  if not job:
+    return "Not Found", 404
+  return render_template('jobpage.html', job=job)
 
 
 if __name__ == '__main__':
