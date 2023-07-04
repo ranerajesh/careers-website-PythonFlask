615018f2affefb4f565cfc7fe64022dc82606527
ranerajesh <rane.rajeshgec@gmail.com>
2023-07-04 14:16:13 +0000

    Adding application form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
@@ -46,6 +46,12 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   return render_template('jobpage.html', job=job)
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  return jsonify(data)
 
 
 if __name__ == '__main__':
