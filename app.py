69494fc9787463645b21e1306df57afcd9f15dd0
ranerajesh <rane.rajeshgec@gmail.com>
2023-07-14 13:28:15 +0000

    Added Job Submission page

--- app.py
+++ app.py
@@ -51,7 +51,10 @@ def show_job(id):
 @app.route("/job/<id>/apply", methods=['post'])
 def apply_to_job(id):
   data = request.form
-  return jsonify(data)
+  job = load_job_from_db(id)
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
 
 
 if __name__ == '__main__':
